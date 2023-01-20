5966ea683dc8805ebc94be5fa52ed18277893457
azulcuellar1 <112140643+azulcuellar1@users.noreply.github.com>
2023-01-19 20:17:09 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL('BirthadyImage.jpg',function(Img){
+        block_image_object=Img
+
+        block_image_object.scaleTowidth(700);
+        block_image_object.scaleToHeoght(510);
+        top:0;
+        left:0;
+    });
+	
+canvas.add(block_image_object);	
+
+
+}
+
+
+
+	
+
